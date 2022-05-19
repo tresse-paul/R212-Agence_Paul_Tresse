208d98bc33c08ea57d58de2e5d2dec790c75d4ed
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-05-19 17:01:15 +0200

    Accueil + app completé

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <template>
-  <header></header>
-
   <RouterView />
 </template>
 
--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,5 +1,50 @@
-<template></template>
+<template>
+  <div class="grid grid-flow-row gap-10 bg-indigo-25">
+    <div class="flex flex-col gap-20">
+      <navigation />
+      <heroMobile />
+    </div>
 
-<script>
-export default {};
+    <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] gap-10 p-4">
+      <card
+        nom="Beverly Springfield"
+        :price="2700"
+        :favori="true"
+        :nbrSdb="2"
+        :nbrC="4"
+        surface="6x7.5"
+        image="/src/assets/Image.jpg"
+        location="2821 Lake Sevilla, Palm Harbor, TX"
+      />
+      <card
+        nom="Palm Harbor"
+        :price="2095"
+        :nbrSdb="2"
+        :nbrC="4"
+        surface="6x7"
+        image="/src/assets/Image3.jpg"
+        location="2699 Green Valley, Highland Lake"
+      />
+      <card
+        nom="Tarpon Bay"
+        :price="4800"
+        :nbrSdb="2"
+        :nbrC="4"
+        surface="6x7.5"
+        image="/src/assets/Image2.jpg"
+        location="103 Lake Shores, Michigan, IN"
+      />
+    </div>
+  </div>
+</template>
+
+<script >
+import card from "../components/card.vue";
+import navigation from "../components/navigation.vue";
+import heroMobile from "../components/heroMobile.vue";
+
+export default {
+  name: "App",
+  components: { card, navigation, heroMobile },
+};
 </script>
