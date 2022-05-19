f0a39cab66651da2f69324a4a43c91b576106e6a
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-05-19 16:55:23 +0200

    icone chevronDown.vue

---
+++ src/components/icons/chevronDown.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex h-5 w-5 items-center justify-center rounded-full bg-indigo-100">
+    <svg width="12" height="12" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
+      <path
+        d="M1.33337 1.66666L6.00004 6.33332L10.6667 1.66666"
+        stroke=""
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  </div>
+</template>
