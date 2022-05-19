3ac42791c38edac5c660b34a49a64ec2d42bd771
tresse-paul <paul.tresse@edu.univ-fcomte.fr>
2022-05-19 16:54:18 +0200

    custom tailwind class + index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      "height": {
+        '4,5': '4.5rem',
+      },
       "colors": {
         // Ici vos couleurs exportées de Figma
+        'indigo': {
+          '25': '#F6F8FF',
+        },
       },
       "fontFamily": {
         // Ici vos familles de polices exportées de Figma
